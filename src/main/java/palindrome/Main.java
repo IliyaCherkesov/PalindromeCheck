4081530775f648e66ae0b1697c29a45bb226494b
IliyaCherkesov <ilya.cherkesov@gmail.com>
2024-02-06 14:04:25 +0200

    Completed task with adding the simple and easy to use menu. Added printMenu function for better readability

--- src/main/java/palindrome/Main.java
+++ src/main/java/palindrome/Main.java
@@ -5,7 +5,41 @@ import java.util.Scanner;
 public class Main
 {
 
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            boolean doBreak = false;
+            printMenu();
+            String str = scanner.nextLine();
+            switch(str.trim()) {
+                case "1":
+                    System.out.println("Please, enter to string to palindrome check:");
+                    String strToCheck = scanner.nextLine();
+                    if (checkToPalindrome(strToCheck)) {
+                        System.out.println("String is palindrome");
+                    } else {
+                        System.out.println("String is not palindrome");
+                    }
+                    break;
+                default:
+                    doBreak = true;
+                    break;
+            }
+            if (doBreak) {
+                break;
+            }
+        }
+        scanner.close();
+    }
 
+    private static void printMenu()
+    {
+        System.out.println("Please select an option:");
+        System.out.println("1. Check palindrome");
+        System.out.println("2. Exit");
+    }
 
     public static boolean checkToPalindrome(String str)
     {
